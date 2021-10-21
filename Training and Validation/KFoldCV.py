175239cdd27c2517a25e10e8ae528078151d4765
Amanda SR Ho <65751896+Amandahsr@users.noreply.github.com>
2021-10-21 18:19:53 +0800

    Update KFoldCV.py

--- Training and Validation/KFoldCV.py
+++ Training and Validation/KFoldCV.py
@@ -1,45 +1,31 @@
-from DataSet import CellsDataset
-
-#Initialise labels file.
-label_csv = "../Data_main/train_labels.csv"
-
-# Define kfold cross validator
-strat_kfold = StratifiedKFold(n_splits=no_fold, shuffle=True, random_state=42)
-
-# Initialise dataset object
-# dataset = CellsDataset("../Data_main/train_labels.csv", "./Data_main/images") # to fill in arguments
-
-# Getting indexes for stratified k fold
-label_df = pd.read_csv(label_csv)
-X = np.zeros(len(label_df))
-y = label_df['labels']
-
-# Preparing data for training on each fold
+#K-fold cross validation code.
+#Data augmentation is removed until we gauge how much space we have.
 for fold, (train_idx, valid_idx) in enumerate(strat_kfold.split(X, y)):
     print("FOLD {}".format(fold))
 
     train_df = label_df.iloc[train_idx][["Patient_no", "Patient_ID", "labels"]]
     valid_df = label_df.iloc[valid_idx][["Patient_no", "Patient_ID", "labels"]]
-    train_set = CellsDataset(train_df, "./Data_main/images")
-    valid_set = CellsDataset(valid_df, "./Data_main/images")
+    train_set = CellsDataset(train_df, "/content/ZB4171_LeukemiaImageClassification-Ongoing-/Data_main/images")
+    valid_set = CellsDataset(valid_df, "/content/ZB4171_LeukemiaImageClassification-Ongoing-/Data_main/images")
 
-    # Add in augmentation data on training set
-    aug_dir = "../test_augment/"
-    aug_df = augment(rounds=5, op_dir=aug_dir, labels=train_df)
-    aug_set = CellsDataset(aug_df, aug_dir)
-    # concat aug_df with train_df
-    train_aug_set = torch.utils.data.ConcatDataset([train_set, aug_set])
+    #Add augmentation data to original training set.
+    # aug_dir = "../test_augment/"
+    # aug_df = augment(rounds=aug_rounds, op_dir=aug_dir, labels=train_df)
+    # aug_set = CellsDataset(aug_df, aug_dir)
+    # train_aug_set = torch.utils.data.ConcatDataset([train_set, aug_set])
 
-    # dataloader for input into the model (need to integrate into correct format for efficientnet)
+    #Dataloader for input into the model (need to integrate into correct format for efficientnet).
     valid_loader = torch.utils.data.DataLoader(valid_set, batch_size=batch_size, shuffle=True)
-    train_loader = torch.utils.data.DataLoader(train_aug_set, batch_size=batch_size, shuffle=True)
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True)
+    
     # input takes in dictionary format
-    dataloaders = {'training': train_loader,'validation': valid_loader}
+    # this part is combining our CellsDataset class with the input format for effiecientnet
+    dataloaders = {'training': train_loader, 'validation': valid_loader}
+    dataset_sizes = {'training': len(train_set), 'validation': len(valid_set)}
+    class_names = [0, 1] # hard code labels based on labels in csv
+    
+    # train data on n epochs
+    train_model(base_model, criterion, optimizer, lr_scheduler, num_epochs=epochs)
 
-    """
-    - put efficientnet model here
-    - do training here
-    - evaluate loss, accuracy here
-    """
-    # delete the augmented training dir bef training on next fold to save disk space
-    shutil.rmtree(aug_dir)
+    #Delete augmented training directory before next training fold to save disk space.
+    #shutil.rmtree(aug_dir)
