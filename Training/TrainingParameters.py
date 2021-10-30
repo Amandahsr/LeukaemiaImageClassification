53bb1ad44a693f9e01ea6566c3822c7caac67f8e
Amanda SR Ho <65751896+Amandahsr@users.noreply.github.com>
2021-10-31 03:07:59 +0800

    Training Parameters

--- Training/TrainingParameters.py
+++ Training/TrainingParameters.py
@@ -7,3 +7,7 @@ num_workers = 4
 aug_rounds = 5
 CV_states = 42
 epochs = 100 # to change
+
+#Create directory for training results
+!mkdir /content/training_results/weights
+!mkdir /content/training_results/stats
