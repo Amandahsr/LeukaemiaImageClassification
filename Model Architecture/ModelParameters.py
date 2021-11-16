0f16ac47e67d91926788bb79bfa12bc122343d44
Amanda SR Ho <65751896+Amandahsr@users.noreply.github.com>
2021-11-16 17:01:29 +0800

    Delete Model Architecture directory

--- Model Architecture/ModelParameters.py
+++
@@ -1,7 +0,0 @@
-#Model parameters.
-learning_rate = 0.001
-momentum = 0.9
-output_layer = 2
-
-#Use CUDA to run training, else use CPU.
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
